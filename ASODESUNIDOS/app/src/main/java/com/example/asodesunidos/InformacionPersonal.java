89c9d29c8a834c832a77a8cababda81dcfd83e98
adriansm1222 <adriansm1222@gmail.com>
2023-04-12 13:54:14 -0600

    Info personal vista
    Vista de la ventana informacion personal

---
+++ ASODESUNIDOS/app/src/main/java/com/example/asodesunidos/InformacionPersonal.java
@@ -0,0 +1,14 @@
+package com.example.asodesunidos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InformacionPersonal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informacion_personal);
+    }
+}
