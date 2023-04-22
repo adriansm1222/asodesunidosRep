2a4f3935502c998c86dcf46b2b16f14ab3c6b1ee
danidb29 <70125112+danidb29@users.noreply.github.com>
2023-04-22 10:34:04 -0600

    enfermito

--- ASODESUNIDOS/app/src/main/java/com/example/asodesunidos/MainActivity.java
+++ ASODESUNIDOS/app/src/main/java/com/example/asodesunidos/MainActivity.java
@@ -2,17 +2,27 @@ package com.example.asodesunidos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boton = findViewById(R.id.button);
+
         consultar();
+    }
+
+    public void iniciarSesion(View view){
+        Intent intent = new Intent(this, PantallaAdministrativa.class);
+        startActivity(intent);
     }
 
     public void consultar(){
